04b41973d177d8213427eb638cc4665e488f3f05
peter <h28099511@yahoo.com.tw>
2022-09-04 15:02:54 +0800

    Commit(20220904): 1.Add NLP function(DialogFlow) 1-1.DialogFlow key json from GCP. (Project name: Google Map API For IOS). 1-2.Add DialogFlowActivity page. 1-3.Add Sign in information.

--- app/src/main/java/com/vaima/vaimagcs/MainActivity.kt
+++ app/src/main/java/com/vaima/vaimagcs/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.vaima.vaimagcs
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -15,9 +16,14 @@ class MainActivity : AppCompatActivity() {
     // for text view and image view
     lateinit var outputTV: TextView
     lateinit var micIV: ImageView
+    lateinit var dialogFlowBtn: Button
 
     // on below line we are creating a constant value
     private val REQUEST_CODE_SPEECH_INPUT = 1
+
+    //Dialog ui
+    lateinit var account: EditText
+    lateinit var password: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         // initializing variables of list view with their ids.
         outputTV = findViewById(R.id.idTVOutput)
         micIV = findViewById(R.id.idIVMic)
+        dialogFlowBtn = findViewById(R.id.dialogflow)
 
         // on below line we are adding on click
         // listener for mic image view.
@@ -65,8 +72,13 @@ class MainActivity : AppCompatActivity() {
                     )
                     .show()
             }
-        }
-    }
+        }//micIv
+
+        dialogFlowBtn.setOnClickListener{
+            showNLPdialog()
+        }//dialogFlowBtn
+
+    }//onCreate
 
     // on below line we are calling on activity result method.
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -88,7 +100,45 @@ class MainActivity : AppCompatActivity() {
                 outputTV.setText(
                     Objects.requireNonNull(res)[0]
                 )
-            }
-        }
+            }//if
+        }//if
+    }//onActivityResult
+
+    fun showNLPdialog(){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+//        builder.setTitle("Please enter account and password!")
+        // Set up the input
+//        var account = EditText(this)
+//        var password = EditText(this)
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+//        account.setHint("Enter account")
+//        account.inputType = InputType.TYPE_CLASS_TEXT
+//        password.setHint("Enter password")
+//        password.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+//        builder.setView(account)
+//        builder.setView(password)
+//        val inflater = MainActivity().layoutInflater
+        val inflater: View = layoutInflater.inflate(R.layout.dialog_signin, null)
+        account = inflater.findViewById(R.id.username)
+        password = inflater.findViewById(R.id.password)
+        builder.setView(inflater)
+        // Set up the buttons
+        builder.setPositiveButton("Sign in", DialogInterface.OnClickListener { dialog, which ->
+            // Here you get get input text from the Edittext
+            var account_Text = account.text.toString()
+            var password_Text = password.text.toString()
+            if(account_Text=="Peter" && password_Text=="muxdisp1"){
+                Toast.makeText(this, "Login success!", Toast.LENGTH_LONG).show()
+                val intent = Intent(this, DialogFlowActivity::class.java)
+                // To pass any data to next activity
+                intent.putExtra("keyIdentifier", "value1")
+                // start your next activity
+                startActivity(intent)
+            }else Toast.makeText(this, "You input account or password isn't correct! Please try again!", Toast.LENGTH_LONG).show()
+        })
+        builder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
+
+        builder.show()
     }
+
 }
